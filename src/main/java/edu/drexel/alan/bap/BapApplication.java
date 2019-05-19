f0be2e4881a64e571fd0ea6ee0e46408a23c8ba1
user <user@users-mbp.hsd1.nj.comcast.net>
2019-05-18 21:37:50 -0400

    Initial Project for bap

---
+++ src/main/java/edu/drexel/alan/bap/BapApplication.java
@@ -0,0 +1,13 @@
+package edu.drexel.alan.bap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BapApplication.class, args);
+	}
+
+}
