0e0402479782eff1a2548c28494a45782265db98
user <user@users-mbp.hsd1.nj.comcast.net>
2019-05-18 23:41:37 -0400

    Added frontend-maven-plugins

---
+++ src/main/java/edu/drexel/alan/bap/controller/HomeController.java
@@ -0,0 +1,13 @@
+package edu.drexel.alan.bap.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@RequestMapping(value = "/")
+	public String index() {
+		return "index";
+	}
+}
